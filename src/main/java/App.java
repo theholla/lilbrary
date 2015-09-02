991539b7d58cb3e34eb7aa6fd60f395bbc0f469b
Diana Holland & Jennifer Morkunas <student@epicodus.com>
2015-09-02 12:13:24 -0700

    Unit tests for Book and Author pass.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,89 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+  get("/", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/index.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  /* Index --> List of Books */
+  get("/books", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    List<Book> books = Book.all();
+    model.put("books", books);
+    model.put("allAuthors", Author.all());
+    model.put("template", "templates/books.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  /* Index --> List of Authors */
+  get("/authors", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    List<Author> authors = Author.all();
+    model.put("authors", authors);
+    model.put("template", "/templates/authors.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  /* List of books -> POST a new book*/
+  post("/books", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    String name = request.queryParams("title");
+    Book newBook = new Book(name);
+    newBook.save();
+    response.redirect("/books");
+    return null;
+  });
+
+  /* List of authors -> POST a new author*/
+  post("/authors", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    String name = request.queryParams("name");
+    Author newAuthor = new Author(name);
+    newAuthor.save();
+    response.redirect("/authors");
+    return null;
+  });
+
+  /* List of books --> view an individual book */
+  get("/books/:id", (request,response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    int id = Integer.parseInt(request.params("id"));
+    Book book = Book.find(id);
+    model.put("book", book);
+    model.put("allAuthors", Author.all());
+    model.put("template", "templates/book.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+
+  /* List of authors --> view an individual author */
+  get("/authors/:id", (request,response) ->{
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    int id = Integer.parseInt(request.params("id"));
+    Author author = Author.find(id);
+    model.put("author", author);
+    model.put("allBooks", Book.all());
+    model.put("template", "templates/author.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  /* Individual book --> add an author */
+  post("/add_authors", (request, response) -> {
+    int bookId = Integer.parseInt(request.queryParams("book_id"));
+    int authorId = Integer.parseInt(request.queryParams("author_id"));
+    Author author = Author.find(authorId);
+    Book book = Book.find(bookId);
+    book.addAuthor(author);
+    response.redirect("/books/" + bookId);
+    return null;
+  });
+
+
+
+
+
 
   }
 }
--- src/test/java/AppIntegrationTest.java
+++ src/test/java/AppIntegrationTest.java
@@ -21,5 +21,67 @@ public class AppIntegrationTest extends FluentTest {
 
   @ClassRule
   public static DatabaseRule database = new DatabaseRule();
-  
-}
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Library Catalog");
+  }
+
+  @Test
+  public void bookFormIsDisplayed() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add or view a book"));
+    assertThat(pageSource()).contains("Add a new book");
+  }
+
+  @Test
+  public void authorFormIsDisplayed() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add or view an author"));
+    assertThat(pageSource()).contains("Add a new author");
+  }
+
+  @Test
+  public void bookTitleIsDisplayedInListWhenCreated() {
+    goTo("http://localhost:4567/books");
+    fill("#title").with("50 Shades of Green");
+    submit(".btn");
+    assertThat(pageSource()).contains("50 Shades of Green");
+  }
+
+  @Test
+  public void authorNameIsDisplayedInListWhenCreated() {
+    goTo("http://localhost:4567/authors");
+    fill("#name").with("JK Rowling");
+    submit(".btn");
+    assertThat(pageSource()).contains("JK Rowling");
+  }
+
+  @Test
+  public void bookHasItsOwnPage() {
+    Book testBook = new Book("Love In The Time of Cholera");
+    testBook.save();
+    goTo("http://localhost:4567/books");
+    click("a", withText("Love In The Time of Cholera"));
+    assertThat(pageSource()).contains("Love In The Time of Cholera");
+  }
+
+  @Test
+  public void authorHasItsOwnPage() {
+    Author testAuthor = new Author("E.L. Jim");
+    testAuthor.save();
+    goTo("http://localhost:4567/authors");
+    click("a", withText("E.L. Jim"));
+    assertThat(pageSource()).contains("E.L. Jim");
+  }
+
+  @Test
+  public void authorIsAddedToBook() {
+
+  }
+
+
+
+
+  }
